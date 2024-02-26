19ef5acfc436a0102bc0f5bb0c624b89a5312ed9
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-26 21:40:17 +0000

    Chart WIP

--- src/app/components/cat-chart/cat-chart.component.ts
+++ src/app/components/cat-chart/cat-chart.component.ts
@@ -12,27 +12,48 @@ import { Chart } from 'chart.js';
 export class CatChartComponent {
   constructor() {}
 
-  ngOninit() {
-    const ctx = document.getElementById('myChart');
+  ngOnInit(): void {
+    this.createChart();
+  }
+  public chart: any;
 
-    new Chart(ctx as any, {
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
       type: 'radar',
       data: {
-        labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
-        datasets: [
-          {
-            data: [20, 10, 4, 2],
-          },
-        ],
+        labels: [
+          'Eating',
+          'Drinking',
+          'Sleeping',
+          'Designing',
+          'Coding',
+          'Cycling',
+          'Running'
+        ], 
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 90, 81, 56, 55, 40],
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(255, 99, 132)'
+        }, {
+          label: 'My Second Dataset',
+          data: [28, 48, 40, 19, 96, 27, 100],
+          fill: true,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgb(54, 162, 235)',
+          pointBackgroundColor: 'rgb(54, 162, 235)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(54, 162, 235)'
+        }]
       },
-      options: {
-        scales: {
-          r: {
-            suggestedMin: 50,
-            suggestedMax: 100,
-          },
-        },
-      },
+      options: { aspectRatio:2.5}
     });
   }
 }
