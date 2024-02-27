df21a4843ce10e8bdaaa7f5eafe7f11c3a31c600
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-27 19:44:50 +0000

    Cat chart updating with selected cat

--- src/app/components/cat-chart/cat-chart.component.ts
+++ src/app/components/cat-chart/cat-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import { CatAPIService } from '../../../services/cat-api.service';
+import { CatNameServiceService } from '../../../services/cat-name-service.service';
 
 Chart.register(...registerables);
 @Component({
@@ -9,27 +10,34 @@ Chart.register(...registerables);
   styleUrl: './cat-chart.component.scss',
 })
 export class CatChartComponent {
-  constructor(private catAPIService: CatAPIService) {}
+  constructor(
+    private catAPIService: CatAPIService,
+    private catNameService: CatNameServiceService
+  ) {}
 
   public chart: any;
   public breed: string = '';
-  
+
   ngOnInit(): void {
-    this.catAPIService.getCats(1).subscribe((data: any[]) => {
-      const breedData = data[0];
-      this.breed = breedData.name;
+    const catName = this.catNameService.getCatName();
+    if (catName) {
+      this.catAPIService.getCatByName(catName).subscribe((data: any[]) => {
+        const breedData = data[0];
+        this.breed = breedData.name;
 
-      const chartData = [
-        breedData.affection_level,
-        breedData.dog_friendly,
-        breedData.child_friendly,
-        breedData.stranger_friendly,
-        breedData.vocalisation,
-        breedData.intelligence
-      ];
-
-      this.createChart(chartData);
-    });
+        // Populate chartData with the required data
+        const chartData = [
+          breedData.affection_level,
+          breedData.dog_friendly,
+          breedData.child_friendly,
+          breedData.stranger_friendly,
+          breedData.vocalisation,
+          breedData.intelligence,
+        ];
+        debugger;
+        this.createChart(chartData);
+      });
+    }
   }
 
   //builds chart to show cat stats provided in labels
@@ -67,12 +75,11 @@ export class CatChartComponent {
             },
             beginAtZero: true,
             min: 0,
-            max: 8
+            max: 8,
           },
         },
         aspectRatio: 1,
       },
-      
     });
   }
 }
