435da27785fcd666c9a6df1e2727ccc0dfff7214
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-27 14:52:50 +0000

    Error case for getting gats for spinner function

--- src/app/components/cat-carousel/cat-carousel.component.ts
+++ src/app/components/cat-carousel/cat-carousel.component.ts
@@ -18,11 +18,23 @@ export class CatCarouselComponent {
   //load cats for spinner randomly by getting a number of cats provided in the limit argument, and presents the number of entries with the latter argument
   getCatsForSpinner(limit: number, numberOfEntries: number): void {
     this.catAPIService.getCats(limit).subscribe((data: any[]) => {
+      // for the event of getting an entry without the required properties, we count the entries so we can add another one in case of error
+      let entriesAdded = 0;
       // Get a random index
       for (let i = 0; i < numberOfEntries; i++) {
         const randomIndex = Math.floor(Math.random() * data.length);
         // Get a random entry from the data array
         const randomEntry = data[randomIndex];
+
+        // Check if the required fields are present in the random entry
+        if (randomEntry.name && randomEntry.description && randomEntry.image && randomEntry.image.url) {
+          this.details.push({
+            name: randomEntry.name,
+            description: randomEntry.description,
+            image: randomEntry.image.url,
+          });
+          entriesAdded++;
+        }
         this.details.push({
           name: randomEntry.name,
           description: randomEntry.description,
