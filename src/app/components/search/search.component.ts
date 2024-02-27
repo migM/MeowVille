80a71ff26def6c6d6ea6bc6f69ea7d3594e22d03
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-27 16:49:37 +0000

    Documentation for search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,23 +4,23 @@ import { CatAPIService } from '../../../services/cat-api.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
   searchQuery: string = '';
   searchResults: any[] = [];
 
-  constructor(private catAPIService: CatAPIService) { }
+  constructor(private catAPIService: CatAPIService) {}
 
+  //fetches cats based on user input
   search(searchQuery: string) {
     this.searchQuery = searchQuery;
     if (this.searchQuery.trim() !== '') {
-      this.catAPIService.getCatByName(searchQuery)
-        .subscribe((data: any[]) => {
-          this.searchResults = data.filter(cat => cat.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
-          console.log(data);
-          // debugger;
-        });
+      this.catAPIService.getCatByName(searchQuery).subscribe((data: any[]) => {
+        this.searchResults = data.filter((cat) =>
+          cat.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        );
+      });
     }
   }
 }
