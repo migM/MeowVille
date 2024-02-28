263536fb6331aa64abccf25f48e856e4c35e100f
migM <m_n_mendes@hotmail.com>
2024-02-28 19:44:51 +0000

    Cat carousel unit tests

--- src/app/components/cat-carousel/cat-carousel.component.spec.ts
+++ src/app/components/cat-carousel/cat-carousel.component.spec.ts
@@ -1,17 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { CatAPIService } from '../../../services/cat-api.service';
+import { CatNameService } from '../../../services/cat-name-service.service';
 import { CatCarouselComponent } from './cat-carousel.component';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('CarouselComponent', () => {
+describe('CatCarouselComponent', () => {
   let component: CatCarouselComponent;
   let fixture: ComponentFixture<CatCarouselComponent>;
+  let catAPIServiceStub: Partial<CatAPIService>;
+  let catNameServiceStub: Partial<CatNameService> & { setCatName: (catName: string) => void };
+
 
   beforeEach(async () => {
+    catAPIServiceStub = {
+      getCats: () => of([
+        { name: 'Cat 1', description: 'Description 1', image: { url: 'image1.jpg' } },
+        { name: 'Cat 2', description: 'Description 2', image: { url: 'image2.jpg' } }
+      ])
+    };
+
+    catNameServiceStub = {
+      setCatName: () => { }
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [CatCarouselComponent]
-    })
-    .compileComponents();
-    
+      declarations: [CatCarouselComponent],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        { provide: CatAPIService, useValue: catAPIServiceStub },
+        { provide: CatNameService, useValue: catNameServiceStub }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CatCarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +47,20 @@ describe('CarouselComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load cats for spinner', () => {
+    component.getCatsForSpinner(50, 2);
+    expect(component.details.length).toEqual(7);
+  });
+
+  it('should set cat name and navigate to details page', () => {
+    const setCatNameSpy = spyOn(catNameServiceStub, 'setCatName');
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigate');
+  
+    component.seeCatDetails('Cat 1');
+  
+    expect(setCatNameSpy).toHaveBeenCalledWith('Cat 1');
+    expect(routerSpy).toHaveBeenCalledWith(['/details'], { queryParams: { catName: 'Cat 1' } });
+  });
+  
 });
