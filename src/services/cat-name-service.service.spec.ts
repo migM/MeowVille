f63586dc00a30ae1e719a3f2d34cff951a68d1a5
migM <m_n_mendes@hotmail.com>
2024-02-28 18:15:45 +0000

    Cat name service unit tests

--- src/services/cat-name-service.service.spec.ts
+++ src/services/cat-name-service.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { CatNameService } from './cat-name-service.service';
 
-describe('CatNameServiceService', () => {
+describe('CatNameService', () => {
   let service: CatNameService;
 
   beforeEach(() => {
@@ -12,5 +12,15 @@ describe('CatNameServiceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set and get cat name', () => {
+    const catName = 'Fluffy';
+    service.setCatName(catName);
+    expect(service.getCatName()).toEqual(catName);
+  });
+
+  it('should return empty string if cat name is not set', () => {
+    expect(service.getCatName()).toEqual('');
   });
 });
