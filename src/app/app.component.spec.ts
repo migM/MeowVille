7ca79625c501faf28c314dbf75747f8b3b54b2a9
migM <m_n_mendes@hotmail.com>
2024-02-28 18:04:33 +0000

    Unit tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+  let location: Location;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
+      declarations: [AppComponent],      
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
       ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule]
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should return true for displayBackButton if current route is not home', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/other');
+    expect(component.displayBackButton()).toBeTrue();
+  });
+
+  it('should return false for displayBackButton if current route is home', () => {
+    spyOnProperty(router, 'url', 'get').and.returnValue('/home');
+    expect(component.displayBackButton()).toBeFalse();
+  });
+
+  it('should navigate back when backClicked is called', () => {
+    const locationSpy = spyOn(location, 'back').and.stub();
+
+    component.backClicked();
+    expect(locationSpy).toHaveBeenCalled();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'MeowVille'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MeowVille');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MeowVille');
+    expect(app.title).toEqual('MeowVille');
   });
 });
