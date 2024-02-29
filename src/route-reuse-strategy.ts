86c618744deec0cce8117606fd81454a06c7c29e
migM <m_n_mendes@hotmail.com>
2024-02-29 12:06:18 +0000

    Documentation

--- src/route-reuse-strategy.ts
+++ src/route-reuse-strategy.ts
@@ -1,5 +1,8 @@
 import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
 
+// I use this approach to fix an issue with the carousel when navigating out and back into the home page which makes the carousel not render. 
+// It might be an issue with CoreUI (the package used for the carousel) I couldn't find info on it or a better fix in time, but 
+// this was a way I got it to work, by reusing the component rendered on entry
 export class CustomRouteReuseStrategy implements RouteReuseStrategy {
   private handlers: { [key: string]: DetachedRouteHandle } = {};
   private readonly homeComponentRoutePath = 'home'; 
